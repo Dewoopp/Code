7dfb0319edc4efa8bd7094ab3afad7df51d4a814
Dewoopp <61980310+Dewoopp@users.noreply.github.com>
2022-03-18 12:20:39 +0000

    Lots of work on the documentation
    New documents are just different versions - need to some out.
    Added new test cases for different things.

--- Tests/cardDecktest.py
+++ Tests/cardDecktest.py
@@ -4,7 +4,7 @@ import statistics
 
 class TestCardDeck(unittest.TestCase):
 
-    def testDeck(self):
+    def testDeckLen(self):
         testDeck = CardDeck()
         self.assertEqual(len(testDeck.cards), 52)
 
@@ -15,7 +15,7 @@ class TestCardDeck(unittest.TestCase):
         testDeck.removeTop()
         self.assertFalse(tempCard in testDeck.cards)
 
-    def testDeckRemove(self):
+    def testDeckLenRemove(self):
         testDeck = CardDeck()
         testDeck.removeTop()
         self.assertEqual(len(testDeck.cards), 51)
@@ -23,12 +23,19 @@ class TestCardDeck(unittest.TestCase):
 
     def testDeckRandomness(self):
         testDeck = CardDeck()
+        # Creates a new array with the cards represented as values from 0 - 51
         cardListNum = [self.cardVal(card, testDeck) for card in testDeck.cards]
-        cardListDiff = [cardListNum[i + 1] - cardListNum[i] for i in range(len(cardListNum) - 1)]
-        self.assertTrue(-5 < statistics.mean(cardListDiff) < 5)
-        stDev = statistics.stdev(cardListDiff)
-        self.assertTrue(stDev > 10)
+        # Creates a new array storing the differences between cards that are adjacent in the cards list
+        cardListDiff = [abs(cardListNum[i + 1] - cardListNum[i]) for i in range(len(cardListNum) - 1)]
+        # Takes the mean of these differences and asserts that it is high
+        # This means that similar cards are far apart on average
+        self.assertTrue(statistics.mean(cardListDiff) > 13)
+        # Takes the standard deviation and asserts that it is high
+        # This means that the spread of the cards is very varied
+        # This is similar to true randomness
+        self.assertTrue(statistics.stdev(cardListDiff) > 10)
     
+    # Gives a value to the given card
     def cardVal(self, card, cardDeck):
         suitNum = cardDeck.suits.index(card.suit)
         return suitNum * 13 + card.number - 1
--- Tests/gameStatetest.py
+++ Tests/gameStatetest.py
@@ -1,13 +1,24 @@
 import unittest
 from GameState import GameState
+from CardStack import CardStack
 
 class TestGameState(unittest.TestCase):
 
-    def stateTest(self):
-        #data = [4, 19, 2, 8, 1]
-        #dataSorted = sorted(data)
-        #insertionSort(data)
-        self.assertEqual(1, 1)
+    def testStateStackLen(self):
+        testState = GameState(None)
+        for i in range(len(testState.cardStacks)):
+            self.assertEqual(len(testState.cardStacks[i].cards), i + 1)
+
+    def testStateStackBackNum(self):
+        testState = GameState(None)
+        for i in range(len(testState.cardStacks)):
+            self.assertEqual(testState.cardStacks[i].backNum, i)
+
+    def testStateWin(self):
+        testState = GameState(None)
+        testState.cardStacks = [CardStack(0) for i in range(testState.numCardStacks)]
+        self.assertEqual(testState.isGameOver(), True)
+
 
 
 if __name__ == '__main__':
